1ad6258217d8d57403c1408620706b86c1151fc9
Georg Ofenbeck <georg.ofenbeck@swisscom.com>
2018-12-14 14:40:36 +0100

    latest changes from today

--- nmmlogic/src/main/scala/Board.scala
+++ nmmlogic/src/main/scala/Board.scala
@@ -1,57 +1,101 @@
 
+sealed abstract class Chip {
+  override def toString: String
+}
 
-
-
-sealed abstract class Chip
-
-final class NoChip extends Chip
-final class BlackChip extends Chip
-final class WhiteChip extends Chip
-
-//case class Node(state: Chip = new NoChip)
-
-
+case object NoChip extends Chip {
+  override def toString: String = "."
+}
+case object BlackChip extends Chip {
+  override def toString: String = "B"
+}
+case object WhiteChip extends Chip {
+  override def toString: String = "W"
+}
 
 case class NineMen(
-                  topLeft: Chip = new NoChip,
-                  topMiddle: Chip = new NoChip,
-                  topRight: Chip = new NoChip,
-                  middleLeft: Chip = new NoChip,
-                  middleRight: Chip = new NoChip,
-                  bottomLeft: Chip = new NoChip,
-                  bottomRight: Chip = new NoChip,
-                  bottomMiddle: Chip = new NoChip
+                  topLeft: Chip = NoChip,
+                  topMiddle: Chip = NoChip,
+                  topRight: Chip = NoChip,
+                  middleLeft: Chip = NoChip,
+                  middleRight: Chip = NoChip,
+                  bottomLeft: Chip = NoChip,
+                  bottomMiddle: Chip = NoChip,
+                  bottomRight: Chip = NoChip
                   )
 
 
 
 
-object Board{
+
+object Board {
+
+    sealed abstract class Position(val value: Int)
+
+    case object  outerTopLeft extends Position(0)
+    case object  outerTopMiddle extends Position(1)
+    case object  outerTopRight extends Position( 2 )
+    case object  outerMiddleLeft extends Position( 3)
+    case object  outerMiddleRight extends Position( 4)
+    case object  outerBottomLeft extends Position( 5)
+    case object  outerBottomMiddle extends Position( 6)
+    case object  outerBottomRight extends Position( 7)
+
+    case object  middleTopLeft extends Position( 8)
+    case object  middleTopMiddle extends Position( 9)
+    case object  middleTopRight extends Position( 10)
+    case object  middleMiddleLeft extends Position( 11)
+    case object  middleMiddleRight extends Position( 12)
+    case object  middleBottomLeft extends Position( 13)
+    case object  middleBottomMiddle extends Position( 14)
+    case object  middleBottomRight extends Position( 15)
+
+    case object  innerTopLeft extends Position( 16)
+    case object  innerTopMiddle extends Position( 17)
+    case object  innerTopRight extends Position( 18)
+    case object  innerMiddleLeft extends Position( 19)
+    case object  innerMiddleRight extends Position( 20)
+    case object  innerBottomLeft extends Position( 21)
+    case object  innerBottomMiddle extends Position( 22)
+    case object  innerBottomRight extends Position( 23)
+
+
+
+  
+  val allPositions = Vector( outerTopLeft ,
+    outerTopMiddle ,
+    outerTopRight ,
+    outerMiddleLeft ,
+    outerMiddleRight ,
+    outerBottomLeft ,
+    outerBottomMiddle ,
+    outerBottomRight ,
+
+    middleTopLeft ,
+    middleTopMiddle ,
+    middleTopRight ,
+    middleMiddleLeft ,
+    middleMiddleRight ,
+    middleBottomLeft ,
+    middleBottomMiddle ,
+    middleBottomRight ,
+
+    innerTopLeft ,
+    innerTopMiddle ,
+    innerTopRight ,
+    innerMiddleLeft ,
+    innerMiddleRight ,
+    innerBottomLeft ,
+    innerBottomMiddle ,
+    innerBottomRight )
+
   val one_rec: Int = 8
-  val nr_recs:Int  = 3
+  val nr_recs: Int = 3
   val spots: Int = one_rec * nr_recs
 
-  def empty(): Board = {
-    val v = for (i <- 0 until Board.spots) yield new NoChip
-    Board()
-  }
-}
 
-case class Board(v: Vector[Chip]) {
-  require(v.length == Board.spots)
-  def ringfromVector(v: Vector[Chip]): NineMen = v match {
-    case Vector(a,b,c, d,e, f,g,h) => NineMen(a,b,c,d,e,f,g,h)
-    case _ => {
-      assert(false, "this should not be possible with the enforced length")
-      NineMen()
-    }
-  }
+  def empty(): Board =  Board(Vector.fill(Board.spots)(NoChip))
 
-
-  def toVector(): Vector[Chip] = {
-    ringToVector(outerRing) ++ ringToVector(middleRing) ++ ringToVector(innerRing)
-  }
-  
   def ringToVector(ring: NineMen): Vector[Chip] = {
     Vector(
       ring.topLeft, ring.topMiddle, ring.topRight,
@@ -59,25 +103,83 @@ case class Board(v: Vector[Chip]) {
       ring.bottomLeft, ring.bottomMiddle, ring.bottomRight
     )
   }
-  
 
 
+}
+
+case class Board(v: Vector[Chip]) {
+  import Board._
+  require(v.length == Board.spots)
+  def ringfromVector(v: Vector[Chip]): NineMen = v match {
+    case Vector(a,b,c, d,e, f,g,h) => NineMen(topLeft = a, topMiddle = b,topRight = c,middleLeft = d,middleRight = e,bottomLeft = f,bottomMiddle = g,bottomRight = h)
+    case _ => {
+      assert(false, "this should not be possible with the enforced length")
+      NineMen()
+    }
+  }
+
+  def getOptions(): Vector[Board.Position] = Board.allPositions.filter(pos => getPos(pos) == NoChip)
+
+  def toVector(): Vector[Chip] = {
+    ringToVector(outerRing) ++ ringToVector(middleRing) ++ ringToVector(innerRing)
+  }
+
+
+  override def toString: String = {
+
+    // 0 1 2 3 4 5 6 7 8 9 101112
+    // x - - - - - x - - - - - x
+    // |           |           |
+    // |   x - - - x - - - x   |
+    // |   |       |       |   |
+    // |   |   x - x - x   |   |
+    // |   |   |       |   |   |
+    // x - x - x       x - x - x
+    // |   |   |       |   |   |
+    // |   |   x - x - x   |   |
+    // |   |       |       |   |
+    // |   x - - - x - - - x   |
+    // |           |           |
+    // x - - - - - x - - - - - x
+
+    s"""#
+        #${getPos(Board.outerTopLeft)} - - - - - ${getPos(Board.outerTopMiddle)} - - - - - ${getPos(Board.outerTopRight)}
+        #|           |           |
+        #|   ${getPos(Board.middleTopLeft)} - - - ${getPos(Board.middleTopMiddle)} - - - ${getPos(Board.middleTopRight)}   |
+        #|   |       |       |   |
+        #|   |   ${getPos(Board.innerTopLeft)} - ${getPos(Board.innerTopMiddle)} - ${getPos(Board.innerTopRight)}   |   |
+        #|   |   |       |   |   |
+        #${getPos(Board.outerMiddleLeft)} - ${getPos(Board.middleMiddleLeft)} - ${getPos(Board.innerMiddleLeft)}       ${getPos(Board.outerMiddleRight)} - ${getPos(Board.middleMiddleRight)} - ${getPos(Board.innerMiddleRight)}
+        #|   |   |       |   |   |
+        #|   |   ${getPos(Board.innerBottomLeft)} - ${getPos(Board.innerBottomMiddle)} - ${getPos(Board.innerBottomRight)}   |   |
+        #|   |       |       |   |
+        #|   ${getPos(Board.middleBottomLeft)} - - - ${getPos(Board.middleBottomMiddle)} - - - ${getPos(Board.middleBottomRight)}   |
+        #|           |           |
+        #${getPos(Board.outerBottomLeft)} - - - - - ${getPos(Board.outerBottomMiddle)} - - - - - ${getPos(Board.outerBottomRight)}
+      """.stripMargin('#')
+  }
 
 
   val outerRing: NineMen = ringfromVector(v.slice(0,Board.one_rec))
   val middleRing: NineMen = ringfromVector(v.slice(Board.one_rec,Board.one_rec*2))
   val innerRing: NineMen = ringfromVector(v.slice(Board.one_rec*2,Board.one_rec*3))
 
-  def checkclose(a: Chip, b: Chip, c: Chip): Boolean = (a,b,c) match{
-    case (x: WhiteChip, y: WhiteChip, z: WhiteChip) => true
-    case (x: BlackChip, y: BlackChip, z: BlackChip) => true
-    case _ => true
+
+  def setPos(pos: Board.Position, state: Chip): Board = Board(v.updated(pos.value,state))
+  def getPos(pos: Board.Position): Chip = v(pos.value)
+
+  def checkclose(a: Chip, b: Chip, c: Chip): Boolean = (a,b,c) match {
+
+    case (x: WhiteChip.type, y: WhiteChip.type, z: WhiteChip.type) => true
+    case (x: BlackChip.type, y: BlackChip.type, z: BlackChip.type) => true
+    case _ => false
   }
 
   def checkTop(ring: NineMen): Boolean = checkclose(ring.topLeft,ring.topMiddle, ring.topRight)
   def checkBottom(ring: NineMen): Boolean = checkclose(ring.bottomLeft, ring.bottomMiddle, ring.bottomRight)
   def checkLeft(ring: NineMen): Boolean = checkclose(ring.topLeft, ring.middleLeft, ring.bottomLeft)
   def checkRight(ring: NineMen): Boolean = checkclose(ring.topRight, ring.middleRight, ring.bottomRight)
+
   def checkRing(ring: NineMen): Boolean = checkTop(ring) || checkBottom(ring) || checkLeft(ring) || checkRight(ring)
 
   def checkCrossLeft: Boolean = checkclose(innerRing.middleLeft, middleRing.middleLeft, outerRing.middleLeft)
--- nmmlogic/src/test/scala/BoardTest.scala
+++ nmmlogic/src/test/scala/BoardTest.scala
@@ -1,12 +1,82 @@
-import org.scalacheck.Properties
+import org.scalacheck.{Gen, Properties}
 import org.scalacheck.Prop.forAll
 
 
 object BoardTest extends Properties("Board") {
 
-  val b = new Board()
 
-  property("no match with less then 3") = forAll{
-
+  def genEmptyBoard(): Gen[Board] = {
+    Board.empty()
   }
+
+
+  def genPlaceOne(board: Board, color: Chip): Gen[Board] = {
+    for {
+      pick <- Gen.oneOf(board.getOptions())
+    } yield board.setPos(pick,color)
+  }
+
+  def genPlaceN(board: Board, n: Int, color: Chip): Gen[Board] = {
+    if (n <= 0) board
+    else {
+      for {
+        onePlacement <- genPlaceOne(board,color)
+        next <- genPlaceN(onePlacement,n-1,color)
+      } yield next
+    }
+  }
+
+
+  def genRandPlacedBoard(w: Int, b: Int): Gen[Board] = {
+    for {
+      whiteplaced <- genPlaceN(Board.empty(),w,WhiteChip)
+      bothplaced <- genPlaceN(whiteplaced,b,BlackChip)
+    } yield bothplaced
+  }
+
+  def genRandBoard(): Gen[Board] = {
+    val b = Board.empty()
+    for {
+      totalchip <- Gen.chooseNum[Int](0,Board.spots)
+      white <- Gen.chooseNum(0, totalchip)
+      black <- Gen.chooseNum(0, totalchip-white)
+      board <- (genRandPlacedBoard(white,black))
+    } yield  board
+  }
+
+  def genMatchBoard(color: Either[WhiteChip.type,BlackChip.type]): Gen[Board] = {
+    /*
+    val c: Chip = color.fold[Chip](fa => fa,fb => fb)
+
+    val matches = Vector(
+      Board.empty().setPos(Board.outerTopLeft,c).setPos(Board.outerTopMiddle,c).setPos(Board.outerTopRight,c),
+      Board.empty().setPos(Board.middleTopLeft,c).setPos(Board.middleTopMiddle,c).setPos(Board.middleTopRight,c),
+      Board.empty().setPos(Board.innerTopLeft,c).setPos(Board.innerTopMiddle,c).setPos(Board.innerTopRight,c),
+
+      Board.empty().setPos(Board.outerBottomLeft,c).setPos(Board.outerBottomMiddle,c).setPos(Board.outerBottomRight,c),
+      Board.empty().setPos(Board.middleBottomLeft,c).setPos(Board.middleBottomMiddle,c).setPos(Board.middleBottomRight,c),
+      Board.empty().setPos(Board.innerBottomLeft,c).setPos(Board.innerBottomMiddle,c).setPos(Board.innerBottomRight,c),
+
+      Board.empty().setPos(Board.outerBottomLeft,c).setPos(Board.outerBottomMiddle,c).setPos(Board.outerBottomRight,c),
+      Board.empty().setPos(Board.middleBottomLeft,c).setPos(Board.middleBottomMiddle,c).setPos(Board.middleBottomRight,c),
+      Board.empty().setPos(Board.innerBottomLeft,c).setPos(Board.innerBottomMiddle,c).setPos(Board.innerBottomRight,c)
+    ) */ ???
+  }
+
+  val randomBoard = genRandPlacedBoard(1,0)
+
+
+  property("toVector / fromVector symetry") = forAll(randomBoard)(board => {
+    val v = board.toVector()
+    val board2 = Board(v)
+    board == board2
+  })
+
+  val twoofEach = genRandPlacedBoard(2,2)
+  property("2 of each cannot check") = forAll(twoofEach)(board => !board.check)
+
+
+
+  //property("no match with less then 3") = forAll(g1: emptyGen)()
+
 }
